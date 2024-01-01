6be6dff1a016387ec9e7e72f2f65385a0b3ce1ed
yevheniydomin <yevhen.domin.uk@gmail.com>
2024-01-01 16:25:11 +0000

    aws cdk init

---
+++ test/lms-mvp.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LmsMvp from '../lib/lms-mvp-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/lms-mvp-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LmsMvp.LmsMvpStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
